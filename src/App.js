175b7ff38d4da994424ba2a60beb54266ecf39b1
Sakshi Nayan <sakshi12princi@gmail.com>
2022-08-08 21:29:56 +0530

    calculator done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./calculator.css";
 
 function App() {
+  const [result , setResult] = useState("")
+    const handleClick =(e)=>{
+        setResult(result.concat(e.target.name))
+    }
+    const Clear=()=>{
+      setResult("");
+    }
+    const Calculate =()=>{
+      setResult(eval(result).toString());
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+  
+    <div id='calculator'>
+        <h1>CALCULATOR</h1>
+        
+        <div id='cal-body'>
+          <div className='formbody'>
+            <form id>
+              <input id='input' type="text" value={result}></input>
+              <button className='eachNum' id="clear" onClick={Clear}>C</button>
+            </form>
+          </div>
+       
+            <div>
+                <button className='eachNum' name='1'  onClick={handleClick}>1</button>
+                <button className='eachNum' name='2' onClick={handleClick}>2</button>
+                <button className='eachNum' name='3' onClick={handleClick}>3</button>
+                <button className='eachNum' name='/' onClick={handleClick}>&divide;</button>
+            </div>
+            <div>
+                <button className='eachNum' name='4' onClick={handleClick}>4</button>
+                <button className='eachNum' name='5' onClick={handleClick}>5</button>
+                <button className='eachNum' name='6' onClick={handleClick}>6</button>
+                <button className='eachNum' name='-' onClick={handleClick}>&ndash;</button>
+            </div>
+            <div>
+                <button className='eachNum' name='7' onClick={handleClick}>7</button>
+                <button className='eachNum' name='8' onClick={handleClick}>8</button>
+                <button  className='eachNum' name='9' onClick={handleClick}>9</button>
+                <button className='eachNum' name='+' onClick={handleClick}>+</button>
+            </div>
+            <div>
+                <button  className='eachNum' name='.' onClick={handleClick}>.</button>
+                <button className='eachNum' name='0' onClick={handleClick} >0</button>
+                <button className='eachNum' name='=' onClick={Calculate} >=</button>
+                <button className='eachNum' name='*' onClick={handleClick}>&times;</button>
+            </div>
+            
+        </div>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
